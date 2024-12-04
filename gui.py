cdca182015c5a45dffaef6306e926ab18c6bc8df
SumitKaware <sumit.kaware3@gmail.com>
2024-12-04 18:25:54 +0530

    first commit

---
+++ gui.py
@@ -0,0 +1,11 @@
+import FreeSimpleGUI as sg
+import functions
+
+
+label = sg.Text("Type in a ToDo")
+input_box = sg.InputText(tooltip="Enter ToDo")
+add_button = sg.Button("Add")
+
+window = sg.Window('My ToDo App',layout=[[label],[input_box, add_button]])
+window.read()
+window.close()
