24a0abfe467f0911d389bac62318cc269e11facc
SumitKaware <sumit.kaware3@gmail.com>
2024-12-05 12:48:10 +0530

    commit 02

--- gui.py
+++ gui.py
@@ -6,6 +6,8 @@ label = sg.Text("Type in a ToDo")
 input_box = sg.InputText(tooltip="Enter ToDo")
 add_button = sg.Button("Add Item")
 
-window = sg.Window('My ToDo App',layout=[[label],[input_box, add_button]])
+window = sg.Window('My ToDo App',
+                   layout=[[label],[input_box, add_button]],
+                   font=('Helvetica',20))
 window.read()
 window.close()
